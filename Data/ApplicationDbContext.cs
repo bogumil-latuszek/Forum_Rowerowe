2cdae47ee793ec6e336fd34b7c27943bd05d0614
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-11 15:19:38 +0100

    dodana migracja: add-migration CreatePosts
    By migracja zadziałała trzeba dodać w ApplicationDbContext:
    public DbSet<Post> Posts { get; set; }
    i dopiero wywołać
    add-migration
    potem wywołuję  update-database
    ono tworzy bazę aplikując wszystkie migracje
    ale nie rusza żadnych plików projektu

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ForumRowerowe.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace ForumRowerowe.Data
             : base(options)
         {
         }
+        public DbSet<Post> Posts { get; set; }
     }
 }
---
+++ Data/Migrations/20211211141157_CreatePosts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumRowerowe.Data.Migrations
+{
+    public partial class CreatePosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
