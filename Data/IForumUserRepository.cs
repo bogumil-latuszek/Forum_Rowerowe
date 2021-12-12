bb59f27607c6a5c1bef2a4a824ecc23d7d30046f
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-12 12:15:46 +0100

    dodanie interface-u User Repository

---
+++ Data/IForumUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using ForumRowerowe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumRowerowe.Data
+{
+    interface IForumUserRepository
+    {
+        Post FindPost(int postID);
+        IList<Post> FindByContent(string contentPattern);
+        IList<Post> FindPage(int page, int size);
+    }
+}
