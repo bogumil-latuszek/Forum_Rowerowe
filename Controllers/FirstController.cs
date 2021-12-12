521b7c0879ca0e646d1bf908bf1b86f583d5ac6c
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-12 16:42:01 +0100

    wymiana w kontrolerze - użycie CRUD repository

--- Controllers/FirstController.cs
+++ Controllers/FirstController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ForumRowerowe.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,38 +9,29 @@ namespace ForumRowerowe.Controllers
 {
     public class FirstController : Controller
     {
-        private static int counter = 3;
-        private static List<ForumRowerowe.Models.Post> ListOfPosts =
-        new List<ForumRowerowe.Models.Post>()
+        private IForumCrudRepository repository;
+        public FirstController(IForumCrudRepository repository)
         {
-            new Models.Post(){
-            PostID=1,Content= "cześć właśnie kupiłem rower" },
-            new Models.Post(){
-            PostID=2,Content= "jaki?" },
-            new Models.Post(){
-            PostID=3,Content= "Scott górski z 2015 mało używany, " +
-                "całkiem tanio idzie znaleść podobne na wyprzedażach tutaj w Lublinie" }
-        };
+            this.repository = repository;
+        }        
         public IActionResult Index()
         {
-            return View(ListOfPosts);
+            var allPosts = repository.FindAll();
+            return View(allPosts);
         }
 
         // GET: Post/Create
         public IActionResult Create()
         {
             return View();
-        }
+         }
         // POST: Post/Create
         [HttpPost]
         public async Task<IActionResult> Create([Bind("PostID,Content")] Models.Post post)
         {
             if (ModelState.IsValid)
             {
-                counter++;
-                post.PostID = counter;
-                ListOfPosts.Add(post);
-                return RedirectToAction(nameof(Index));
+                repository.AddPosts(post);
             }
             return RedirectToAction(nameof(Index));
         }
@@ -51,43 +43,18 @@ namespace ForumRowerowe.Controllers
             {
                 return NotFound();
             }
-            foreach (var post in ListOfPosts)
-            {
-                if (post.PostID == id)
-                {
-                    return View();
-                }
-            }
-            return NotFound();
+            int Id = id.GetValueOrDefault();
+            var post = repository.FindPost(Id);
+            return View(post);
         }
 
         // POST: Post/Delete/5
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
         {
-            int? removedPostID = null;
-            foreach (var post in ListOfPosts)
+            if (ModelState.IsValid)
             {
-                if (post.PostID == id)
-                {
-                    removedPostID = post.PostID;
-                    ListOfPosts.Remove(post);
-                    if (counter>0)
-                    {
-                        counter--;
-                    }
-                    break; //dodać zabezpieczenie na wypadek gdyby 2 lub więcej postów miało to samo postID
-                }
-                
-            }
-            if (removedPostID == null) { return RedirectToAction(nameof(Index)); }
-            foreach (var post in ListOfPosts)
-            {
-                if (post.PostID >= removedPostID && post.PostID >= 1)
-                {
-                    post.PostID -= 1;
-                }
-
+                repository.DeletePosts(id);
             }
             return RedirectToAction(nameof(Index));
         }
@@ -95,28 +62,19 @@ namespace ForumRowerowe.Controllers
         // GET: Post/Edit/5
         public async Task<IActionResult> Edit(int id)
         {
-             return View(ListOfPosts[id-1]);
+            var post = repository.FindPost(id);
+            return View(post);
         }
 
         // POST: Post/Edit/5
         [HttpPost]
         public async Task<IActionResult> Edit( [ Bind("PostID,Content")] Models.Post post)
         {
-
             if (ModelState.IsValid)
             {
-                for (int i = 0; i < ListOfPosts.Count; i++)
-                {
-                    if(ListOfPosts[i].PostID == post.PostID)
-                    {
-                        ListOfPosts[i] = post;
-                        break;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                repository.UpdatePosts(post);
             }
             return RedirectToAction(nameof(Index));
         }
-
     }
 }
