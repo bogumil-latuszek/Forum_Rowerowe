674acd445f767fd71057a6bf165eab4c4eca7cc6
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-12 12:31:13 +0100

    dodanie User Repository

---
+++ Data/ForumUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using ForumRowerowe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumRowerowe.Data
+{
+    public class ForumUserRepository
+    {
+        private ApplicationDbContext _context;
+        public ForumUserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Post FindPost(int id)
+        {
+            var item = (from x in _context.Posts where x.PostID == id select x).First();
+            return item;
+        }
+        public IList<Post> FindByContent(string contentPattern)
+        {
+                return (from p in _context.Posts where p.Content.Contains(contentPattern) select p).ToList();
+        }
+        public IList<Post> FindPage(int page, int size)
+        {
+                return (from p in _context.Posts select p).OrderBy(p => p.PostID).Skip((page - 1) * size).Take(size).ToList();
+        }
+    }
+}
