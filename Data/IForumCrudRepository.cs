cd0d681f313a7b28eba94416e2666e27e75b13d1
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-12 12:13:34 +0100

    dodanie interface-u CRUD Repository

---
+++ Data/IForumCrudRepository.cs
@@ -0,0 +1,18 @@
+﻿using ForumRowerowe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumRowerowe.Data
+{
+    public interface IForumCrudRepository
+    {
+        void DeletePosts(int postID);
+        void AddPosts(Post post);
+        Post FindPost(int postID);
+        void UpdatePosts(Post post);
+        IList<Post> FindAll();
+        IList<Post> FindPage(int page, int size);
+    }
+}
