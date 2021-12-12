09fd2d393b39bfb4b57fb4391cd0cc7418d787d2
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-12 12:28:00 +0100

    dodanie repozytorium admin

---
+++ Data/ForumAdminRepository.cs
@@ -0,0 +1,45 @@
+﻿using ForumRowerowe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumRowerowe.Data
+{
+    public class ForumAdminRepository: IForumCrudRepository
+    {
+        private ApplicationDbContext _context;
+        public ForumAdminRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void DeletePosts(int postID)
+        {
+            _context.Posts.Remove(FindPost(postID));
+            _context.SaveChanges();
+        }
+        public Post FindPost(int id)
+        {
+            var post = (from x in _context.Posts where x.PostID == id select x).First();
+            return post;
+        }
+        public void AddPosts(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+        }
+        public void UpdatePosts(Post post)
+        {
+            _context.Posts.Update(post);
+            _context.SaveChanges();
+        }
+        public IList<Post> FindAll()
+        {
+            return _context.Posts.ToList();
+        }
+        public IList<Post> FindPage(int page, int size)
+        {
+            return (from p in _context.Posts select p).OrderBy(p => p.PostID).Skip((page - 1) * size).Take(size).ToList();
+        }
+    }
+}
