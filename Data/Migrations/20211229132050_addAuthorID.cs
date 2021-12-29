a2e669c18611d283a2e2a3ad34f79735a9863d93
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2021-12-29 14:22:41 +0100

    dodana migracja addAuthorID i update-database

---
+++ Data/Migrations/20211229132050_addAuthorID.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumRowerowe.Data.Migrations
+{
+    public partial class addAuthorID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "authorID",
+                table: "Threads",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "authorID",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "authorID",
+                table: "Threads");
+
+            migrationBuilder.DropColumn(
+                name: "authorID",
+                table: "Posts");
+        }
+    }
+}
