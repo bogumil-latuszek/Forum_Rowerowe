15f0e4e65c6896f4d07d2467ca4c74cd19f2250c
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-01-08 20:53:13 +0100

    dodana migracja ThreadIDinPost
    PM> add-migration ThreadIDinPost
    PM> update-database
    
    Pole ThreadID w tablicy Posts nie może być null
    (post zawsze tworzymy z wnętrza jakiegoś wątku)

--- Data/Migrations/20220108194724_ThreadIDinPost.Designer.cs
+++ Data/Migrations/20220108194724_ThreadIDinPost.Designer.cs
@@ -4,14 +4,16 @@ using ForumRowerowe.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ForumRowerowe.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220108194724_ThreadIDinPost")]
+    partial class ThreadIDinPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20220108194724_ThreadIDinPost.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumRowerowe.Data.Migrations
+{
+    public partial class ThreadIDinPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Threads_ThreadID",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ThreadID",
+                table: "Posts",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Threads_ThreadID",
+                table: "Posts",
+                column: "ThreadID",
+                principalTable: "Threads",
+                principalColumn: "ThreadID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Threads_ThreadID",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ThreadID",
+                table: "Posts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Threads_ThreadID",
+                table: "Posts",
+                column: "ThreadID",
+                principalTable: "Threads",
+                principalColumn: "ThreadID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
